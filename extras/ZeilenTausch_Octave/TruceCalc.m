5b32bdb6b94e203fb2d116a7d4d8dfcefdd78fb2
Mauricio <m.gomez@tu-bs.de>
2021-01-15 16:01:28 +0100

    Small code cleaning before putting the algorithm as a function

--- extras/ZeilenTausch_Octave/TruceCalc.m
+++ extras/ZeilenTausch_Octave/TruceCalc.m
@@ -2,8 +2,8 @@
 clc
 clear all
 
-addpath("Functions")
 # Use custom path
+addpath("Functions")
 
 # Get beispiel Knotenmatrix and Elementmatrix
 elementMatrix = [1,2;2,3;4,5;1,4;2,5;2,4;3,5];
@@ -11,35 +11,19 @@ knotenMatrix  = [0,0;270,468;540,0;810,468;1080,0]
 aussenkraefteVektor = [0;0;4;-5;0;0;0;0;0;0];
 lagerVector = [1;2;10];     # Lager in Knoten 1 Y Richtung: 2 und so
 
-
 ######################
-function K_mtrx = buildSystemSteifigkeitsMatrix(knotenAnzahl, kStab_liste, IV)
-  K_mtrx = zeros(knotenAnzahl*2,knotenAnzahl*2);
-  knotenAnzahl
-  for currentStab=1:columns(IV)
-    for j=1:4
-      for k =1:4    
-        #
-        #K_mtrx(IV{currentStab}(j),IV{currentStab}(k)) = 1;
-        K_mtrx(IV{currentStab}(j),IV{currentStab}(k)) += kStab_liste{currentStab}(j,k);
-      endfor
-    endfor
-  endfor
-endfunction
-
-v = zeros(rows(knotenMatrix)*2,1); #do something with this in the future
-elementIndexVector = cell(1,rows(elementMatrix));
-
+# Bilde Liste der Elementsteifigkeiten
 kStab_liste = getk_pseudo_hypermatrix(elementMatrix, knotenMatrix);
 
+# Bilde Element Indexvektor
+elementIndexVector = cell(1,rows(elementMatrix));
 for i=1:columns(elementIndexVector)   # Staying in the same row (looping thrugh adjacent array elements) might be more CPU efficient cause
   startKnoten = elementMatrix(i,1);
   endKnoten   = elementMatrix(i,2);
   elementIndexVector(1,i) = [2*startKnoten-1,2*startKnoten,2*endKnoten-1,2*endKnoten];
 endfor
 
-
-
+# Bilde Systemsteifigkeitsmatrix
 K_matrx = buildSystemSteifigkeitsMatrix(rows(knotenMatrix), kStab_liste, elementIndexVector);
 
 # Bringe Lagerknoten runter
@@ -47,9 +31,9 @@ neweq = conservativeRowBottomPush(K_matrx,lagerVector);
 K_matrx = neweq{1};
 multiplicand_sorting_vector= neweq{2};
 
+# Extract submatrices from Steifigkeitsmatrix and save them as individual variables
 K_matrx_submatrices = extractSubmatricesFromK(K_matrx, rows(lagerVector));
 
-# Extrahiere Submatrizen von K
 K_matrx_11 = K_matrx_submatrices{1,1};
 K_matrx_12 = K_matrx_submatrices{1,2};
 K_matrx_21 = K_matrx_submatrices{2,1};
