04c1d69e6c8ffc3f9a7a08e47d0e86710f13f846
Mauricio <m.gomez@tu-bs.de>
2021-01-15 11:28:41 +0100

    Changed Knoten Reihenfolge to optmized

--- extras/ZeilenTausch_Octave/TruceCalc.m
+++ extras/ZeilenTausch_Octave/TruceCalc.m
@@ -6,13 +6,13 @@ clear all
 addpath("Functions")
 
 # Get beispiel Knotenmatrix and Elementmatrix
-knotenMatrix = getKnotenMatrix_bsp();
-elementMatrix = getElementMatrix_bsp();
-aussenkraefteVektor = [0;0;0;0;0;0;4;-5;0;0];
-lagerVector = [1;2;6];     # Lager in Knoten 1 Y Richtung: 2 und so
+elementMatrix = [1,2;2,3;4,5;1,4;2,5;2,4;3,5]
+knotenMatrix  = [0,0;270,468;540,0;810,468;1080,0]
+aussenkraefteVektor = [0;0;4;-5;0;0;0;0;0;0]
+lagerVector = [1;2;10];     # Lager in Knoten 1 Y Richtung: 2 und so
 
 
 ######################
-dbstop in "berechnungLagerkraefte" at 37
+#dbstop in "berechnungLagerkraefte" at 37
 erg = berechnungLagerkraefte(knotenMatrix, elementMatrix, aussenkraefteVektor, lagerVector)
 
